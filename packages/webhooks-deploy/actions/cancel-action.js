7bcd1ece145bf9d0eb14041ba9578e60adf1c1c9
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2019-10-18 18:44:59 +0200

    Move to yarn v2

---
+++ packages/webhooks-deploy/actions/cancel-action.js
@@ -0,0 +1,17 @@
+const { getReporter } = require('../utils/follow-db');
+
+async function action(req, { id }) {
+  if (typeof id !== 'string') return { status: 400, result: `id field must be a string, received ${typeof id}` };
+
+  const reporter = await getReporter(id);
+  if (reporter) {
+    if (!reporter.canCancel()) return { status: 400, result: `Task can't be cancelled. (status: ${reporter.status})` };
+    const isCancelling = reporter.cancelTask();
+    if (!isCancelling) return { status: 500, result: `No task associated with reporter ${id}` };
+    return { status: 200, result: reporter.json() };
+  }
+
+  return { status: 400, result: `Could not find id ${id}` };
+}
+
+module.exports = action;
